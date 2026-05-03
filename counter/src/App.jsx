583fc6d20891894e521ed6c9bf0c5207d84beccf
codeTukai <tukaighosh753@gmail.com>
2026-05-03 23:35:40 +0530

    change

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,9 +3,22 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  let counter = 5
+  const addValue = (e) => {
+    
+  }
   return (
     <>
-    <div>Hello</div>
+    <div>
+      <h1>chai aur code</h1>
+      <h2>chai aur counter:{counter} </h2>
+
+      <button onClick={addValue}>Add value: {counter} </button>
+      <br></br>
+      <br />
+      <button>Remove value</button>
+    </div>
     </>
   )
 }

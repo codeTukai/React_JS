d53b139642e4c551009895c19156e1490a33ddd6
codeTukai <tukaighosh753@gmail.com>
2026-05-28 20:11:17 +0530

    Fix background color and text color in InputBox component

--- currencyConvertor/src/components/InputBox.jsx
+++ currencyConvertor/src/components/InputBox.jsx
@@ -15,9 +15,9 @@ function InputBox({
     const amountInputId = useId()
 
   return (
-    <div className={`bg-blac p-3 rounded-lg text-sm flex ${className}`}>
+    <div className={`bg-gray-500 p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">{label}</label>
+        <label htmlFor={amountInputId} className="text-white mb-2 inline-block">{label}</label>
         <input
           id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
@@ -36,7 +36,7 @@ function InputBox({
           value={selectCurrency}
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
           disabled={currencyDisable}
-          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          className="rounded-lg px-1 py-1 bg-gray-600 cursor-pointer outline-none"
         >
           {currencyOptions.map((currency) => (
             <option key={currency} value={currency}>
